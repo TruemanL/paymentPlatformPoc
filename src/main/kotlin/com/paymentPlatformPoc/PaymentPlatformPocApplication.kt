9caaa6ec84d64e74c3f0d6057032474c998cd353
Trueman Lam <truemanlam@Truemmans-MacBook-Air.local>
2023-01-24 19:54:49 +0900

    initial commit

---
+++ src/main/kotlin/com/paymentPlatformPoc/PaymentPlatformPocApplication.kt
@@ -0,0 +1,11 @@
+package com.paymentPlatformPoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentPlatformPocApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentPlatformPocApplication>(*args)
+}
---
+++ src/test/kotlin/com/paymentPlatformPoc/PaymentPlatformPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paymentPlatformPoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentPlatformPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
