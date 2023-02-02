fe90a68f2bccd2e5c60d7b72a590f460564620b9
Trueman Lam <truemanlam@Truemmans-MacBook-Air.local>
2023-02-02 22:58:12 +0900

    added grpc dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("com.google.protobuf") version "0.9.2"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.7.22"
@@ -19,6 +21,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("net.devh:grpc-spring-boot-starter:2.14.0.RELEASE")
+	implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+	implementation("io.grpc:grpc-protobuf:1.3.0")
+	implementation("com.google.protobuf:protobuf-kotlin:3.21.12")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -36,4 +42,29 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.21.12"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.3.0"
+		}
+		id("grpckt") {
+			artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc")
+				id("grpckt")
+			}
+			it.builtins {
+				id("kotlin")
+			}
+		}
+	}
 }
