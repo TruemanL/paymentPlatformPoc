8913714be2c07afaceea98a4888d07a47f3c7dd3
Trueman Lam <truemanlam@Truemmans-MacBook-Air.local>
2023-02-03 21:31:51 +0900

    refactored type conversion functions to be extension functions

--- src/main/kotlin/com/paymentPlatformPoc/adaptor/PaymentAdaptorImpl.kt
+++ src/main/kotlin/com/paymentPlatformPoc/adaptor/PaymentAdaptorImpl.kt
@@ -1,6 +1,8 @@
 package com.paymentPlatformPoc.adaptor
 
 import com.paymentPlatformPoc.dto.PaymentDto
+import com.paymentPlatformPoc.dto.SalesDto
+import com.paymentPlatformPoc.entity.Sale
 import com.paymentPlatformPoc.grpc.adaptor.*
 import com.paymentPlatformPoc.service.SaleService
 import com.paymentPlatformPoc.util.DateTimeUtil
@@ -10,44 +12,44 @@ import java.math.BigDecimal
 @GrpcService
 class PaymentAdaptorImpl(val saleService: SaleService): PaymentAdaptorGrpcKt.PaymentAdaptorCoroutineImplBase() {
     override suspend fun makePayment(request: PaymentRequest): PaymentResponse {
-        val paymentDto = getPaymentDto(request)
-        return saleService.getSaleIfValid(paymentDto)
+        return saleService.getSaleIfValid(request.toPaymentDto())
             .fold(
                 { errorMessage -> throw Exception(errorMessage) },
                 { sale -> {
                     saleService.save(sale)
-                    PaymentResponse.newBuilder()
-                        .setFinalPrice(sale.transactionPrice.toString())
-                        .setPoints(sale.points)
-                        .build()
+                    sale.toPaymentResponse()
                 } }
             ).invoke()
     }
+
+    private fun PaymentRequest.toPaymentDto(): PaymentDto =
+        PaymentDto(
+            price = BigDecimal(price),
+            priceModifier = BigDecimal(priceModifier),
+            paymentMethod = paymentMethod,
+            dateTime = DateTimeUtil.getLocalDateTimeFromIsoInstantString(datetime) ?: throw Exception()
+        )
+
+    private fun Sale.toPaymentResponse(): PaymentResponse =
+        PaymentResponse.newBuilder()
+            .setFinalPrice(transactionPrice.toString())
+            .setPoints(points)
+            .build()
+
 
     override suspend fun getListOfSalesSummary(request: DateRange): ListOfSalesSummary {
         val startDateTime = DateTimeUtil.getLocalDateTimeFromIsoInstantString(request.startDateTime)
             ?: throw Exception()
         val endDateTime = DateTimeUtil.getLocalDateTimeFromIsoInstantString(request.endDateTime)
             ?: throw Exception()
-        val salesSummary: List<SalesSummary> = saleService.getSalesDtoListInRange(startDateTime, endDateTime)
-            .map {
-                SalesSummary.newBuilder()
-                    .setDatetime(it.datetime)
-                    .setSales(it.sales)
-                    .setPoints(it.points)
-                    .build()
-            }
+        val salesSummary = saleService.getSalesDtoListInRange(startDateTime, endDateTime).map { it.toSalesSummary() }
         return ListOfSalesSummary.newBuilder().addAllSales(salesSummary).build()
     }
 
-    private fun getPaymentDto(paymentRequest: PaymentRequest): PaymentDto {
-        return with(paymentRequest) {
-            PaymentDto(
-                price = BigDecimal(price),
-                priceModifier = BigDecimal(priceModifier),
-                paymentMethod = paymentMethod,
-                dateTime = DateTimeUtil.getLocalDateTimeFromIsoInstantString(datetime) ?: throw Exception()
-            )
-        }
-    }
+    private fun SalesDto.toSalesSummary(): SalesSummary =
+        SalesSummary.newBuilder()
+            .setDatetime(datetime)
+            .setSales(sales)
+            .setPoints(points)
+            .build()
 }
